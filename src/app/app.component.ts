d4211af6eaffa7a94e31c99a347af9730fcbaa1c
Manikandan Thangaraj <mani@agiratech.com>
2017-10-30 13:42:41 +0530

    Exercise Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  validateForm: FormGroup;
+
+
+  constructor(private form: FormBuilder) {
+  	this.validateForm = new FormGroup({
+      'name': new FormControl(),
+      'email': new FormControl(),
+      'age': new FormControl(),
+      'address': new FormGroup({
+        'country': new FormControl(),
+        'city': new FormControl()
+      })
+    });
+  }
+
+
+  register(validateForm: NgForm) {
+    console.log('Registration successful.');
+    console.log(validateForm.value);
+    alert("Hi "+validateForm.value.name+" you information are valid.")
+  }
 }
